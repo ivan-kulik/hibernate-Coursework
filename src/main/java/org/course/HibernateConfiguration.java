6279c32f65f54890f5999a69650f69c9360b1152
Ivan Kulik <ivankulik1978@gmail.com>
2026-02-21 17:07:16 +0300

    feat: create HibernateConfiguration file

---
+++ src/main/java/org/course/HibernateConfiguration.java
@@ -0,0 +1,42 @@
+package org.course;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class HibernateConfiguration {
+
+    @Value(value = "${hibernate.connection.url}")
+    private String connectionURL;
+
+    @Value("${hibernate.connection.username}")
+    private String connectionUsername;
+
+    @Value("${hibernate.connection.password}")
+    private String connectionPassword;
+
+    @Value("${hibernate.show_sql}")
+    private String showSql;
+
+    @Value("${hibernate.hbm2ddl.auto}")
+    private String hbm2ddlAuto;
+
+    @Bean
+    public SessionFactory getSessionFactory() {
+        org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+
+        configuration
+                .addPackage("org.course")
+                .setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
+                .setProperty("hibernate.connection.url", connectionURL)
+                .setProperty("hibernate.connection.username", connectionUsername)
+                .setProperty("hibernate.connection.password", connectionPassword)
+                .setProperty("hibernate.show_sql", showSql)
+                .setProperty("hibernate.hbm2ddl.auto", hbm2ddlAuto);
+        return configuration.buildSessionFactory();
+    }
+}
--- src/main/java/org/course/Main.java
+++ src/main/java/org/course/Main.java
@@ -1,7 +1,13 @@
 package org.course;
 
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("test");
+        AnnotationConfigApplicationContext appContext =
+                new AnnotationConfigApplicationContext("org.course");
+        SessionFactory sessionFactory = appContext.getBean(SessionFactory.class);
     }
 }
