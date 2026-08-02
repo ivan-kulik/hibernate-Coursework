cf9a907b3dd9e6a8962fe4604b8eaafae4a39caf
Ivan Kulik <ivankulik1978@gmail.com>
2026-08-02 22:35:24 +0300

    fix: remove @GeneratedValue anotation from id field in Profile entity

--- src/main/java/org/course/entity/Profile.java
+++ src/main/java/org/course/entity/Profile.java
@@ -3,8 +3,6 @@ package org.course.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
@@ -23,7 +21,6 @@ import lombok.Setter;
 public class Profile {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
     private Long id;
 
