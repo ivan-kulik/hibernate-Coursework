a46b48d23d62deba23a5356f034f874a54260e59
Ivan Kulik <ivankulik1978@gmail.com>
2026-08-02 14:52:47 +0300

    feat: create CommandRegistry component

---
+++ src/main/java/org/course/command/CommandRegistry.java
@@ -0,0 +1,31 @@
+package org.course.command;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Component
+public class CommandRegistry {
+
+    private final Map<ConsoleCommandType, ConsoleCommand> commandMap;
+
+    public CommandRegistry(List<ConsoleCommand> commands) {
+        this.commandMap = commands.stream()
+                .collect(Collectors.toMap(
+                        ConsoleCommand::getType,
+                        Function.identity()
+                ));
+    }
+
+    public ConsoleCommand getCommand(ConsoleCommandType type) {
+        return this.commandMap.get(type);
+    }
+
+    public Collection<ConsoleCommand> getAllCommands() {
+        return commandMap.values();
+    }
+}
