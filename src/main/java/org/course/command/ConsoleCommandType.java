7c1de28c9a09c601418b50958b57212c025e4cc8
Ivan Kulik <ivankulik1978@gmail.com>
2026-08-02 13:38:21 +0300

    feat: create ConsoleCommandType enum

---
+++ src/main/java/org/course/command/ConsoleCommandType.java
@@ -0,0 +1,18 @@
+package org.course.command;
+
+public enum ConsoleCommandType {
+
+    ADD_CLIENT,
+
+    DELETE_CLIENT,
+
+    EDIT_PROFILE,
+
+    ADD_ORDER,
+
+    EDIT_COUPONS,
+
+    FIND_ORDERS,
+
+    EXIT,
+}
