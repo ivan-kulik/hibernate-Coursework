0bf37df02c74c10994d3674a4192773cd3b07a93
Ivan Kulik <ivankulik1978@gmail.com>
2026-08-02 22:53:42 +0300

    feat: create ClientService

---
+++ src/main/java/org/course/service/ClientService.java
@@ -0,0 +1,44 @@
+package org.course.service;
+
+import org.course.entity.Client;
+import org.course.entity.Profile;
+import org.course.repository.ClientRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+
+    private final ClientRepository clientRepository;
+
+    public ClientService(
+            ClientRepository clientRepository
+    ) {
+        this.clientRepository = clientRepository;
+    }
+
+    public Long createClient(
+            String name,
+            String email,
+            String address,
+            String phone
+    ) {
+        Client client = new Client(name, email);
+        Profile profile = new Profile(address, phone);
+        client.setProfile(profile);
+
+        this.clientRepository.save(client);
+        return client.getId();
+    }
+
+    public boolean isClientExistByName(String name) {
+        return this.clientRepository.existsByName(name);
+    }
+
+    public boolean isClientExistByEmail(String email) {
+        return this.clientRepository.existsByEmail(email);
+    }
+
+    public boolean isProfileExistByPhone(String phone) {
+        return this.clientRepository.existsByPhone(phone);
+    }
+}
